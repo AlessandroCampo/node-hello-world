a6c9ebfa9cf6fa86b2dc6cd4fecddc7a633a6047
Alessandro Campo <alessandrocampo97@gmail.com>
2024-05-21 14:42:13 +0200

    feat: server creation and first test

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 require('dotenv').config();
+const http = require('http');
 const port = process.env.PORT || '8080';
 const host = process.env.HOST || 'localhost';
 
+
 console.log(host, port)
+
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, {
+        "Content-Type": "text/html"
+    })
+    res.end('Hello World')
+})
+
+server.listen(port, host, () => {
+    console.log('server running on port ' + port)
+})
