e01b93cf89c33fc021b9f2696e9b1645b579f1da
Alessandro Campo <alessandrocampo97@gmail.com>
2024-05-21 14:45:53 +0200

    feat: returning variable from .env

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 const http = require('http');
 const port = process.env.PORT || '8080';
 const host = process.env.HOST || 'localhost';
+const res = process.env.RES
+const resHTML = `<h1> Welcome to my page! </h1> <p> Here is the variable you were looking for: <strong>${res}</strong> </p>`
 
-
-console.log(host, port)
 
 const server = http.createServer((req, res) => {
 
     res.writeHead(200, {
         "Content-Type": "text/html"
     })
-    res.end('Hello World')
+    res.end(resHTML)
 })
 
 server.listen(port, host, () => {
